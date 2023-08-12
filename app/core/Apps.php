efd70cacc33fad2ede59dd629691e4af6142aa8b
Ahmad Zaelani <myroot593@gmail.com>
2023-08-13 04:14:23 +0700

    Update Apps.php

--- app/core/Apps.php
+++ app/core/Apps.php
@@ -1,21 +1,12 @@
 <?php
 /** 
 														
-	* Waktu yang telah dilewati layaknya sebuah mimpi 
-	* yang takan bisa diulang kembali. Semua hanya terjadi sebentar 
-	* lalu jadi kenangan.
-	* Hidup seperti tidak ada masa depan, karena setiap
-	* masa depan hanya akan menjadi kenangan.
-	* Dan kenangan ini mungkin akan terus hidup dimasa depan - masa depan 
-	* yang juga akan menjadi kenangan :(
-	*
-    *----------------------------------------*
-    * Author          			 			 *
-	* name 			  : ahmadza 			 * 
-	* mail 			  : myroot593@gmail.com  *
-	* blog 			  : root93.co.id         *  
-	* framework name  : zeel php frame 1.0	 *
-	*----------------------------------------*
+	
+	=========================================================
+		zeel php
+		https://github.com/myroot593
+		Copyright (c) 2021 Ahmad Zaelani
+	=========================================================
 	* Apps
 	* ---------------------------------------------------------------
 	* Apps.php adalah bagian bingkai kerja query yang digunakan untuk
@@ -98,21 +89,42 @@
 **/
 
 
-class Apps extends Database
+class Apps extends database
 {
 	
 	
 	
-	public function selectTable($table, $where=null)
+	public function selectTable($table, $where=null, $order=null)
 	{
 		try
 		{	
 			if(!empty($where)):
-				$sql="SELECT * FROM $table WHERE $where";
+				$sql="SELECT * FROM $table WHERE $where $order";
 				$stmt=$this->link->prepare($sql);
 				return $stmt;
 			else:
-				$sql="SELECT * FROM $table";
+				$sql="SELECT * FROM $table $order";
+				$stmt=$this->link->prepare($sql);
+				$stmt->execute();
+				return $stmt;
+			endif;
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+	public function selectTable2($table, $where=null, $getData=null, $order=null)
+	{
+		try
+		{	
+			if(!empty($where)):
+				$sql="SELECT * FROM $table WHERE $where=:$where $order";
+				$stmt=$this->link->prepare($sql);
+				$stmt->bindParam(":$where",$getData);
+				return $stmt;
+			else:
+				$sql="SELECT * FROM $table $order";
 				$stmt=$this->link->prepare($sql);
 				$stmt->execute();
 				return $stmt;
@@ -128,12 +140,30 @@ class Apps extends Database
 		$this->last = $this->link->lastInsertId();
 		return $this->last;
 	}
+	
 	public function insertTable($table, $kolom, $value, $array)
 	{
 		
 		try
 		{
 			$sql="INSERT INTO $table($kolom) VALUES($value)";
+			$stmt=$this->link->prepare($sql);
+			$stmt->execute($array);
+
+			return true;
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+			return false;
+		}
+	}
+	public function replaceTable($table, $kolom, $value, $array)
+	{
+		
+		try
+		{
+			$sql="REPLACE INTO $table($kolom) VALUES($value)";
 			$stmt=$this->link->prepare($sql);
 			$stmt->execute($array);
 
@@ -172,7 +202,7 @@ class Apps extends Database
 		}
 		catch(PDOException $e)
 		{
-			
+			echo $e->getMessage();
 			return false;
 		}
 	}
@@ -213,16 +243,16 @@ class Apps extends Database
 			echo $e->getMessage();
 		}
 	}
-	public function selectTable_limit($table, $start, $finish, $where=null)
+	public function selectTable_limit($table, $start, $finish, $where=NULL, $order=null)
 	{
 		try
 		{
 			if(!empty($where)):
-				$sql="SELECT * FROM $table WHERE $where LIMIT $start, $finish";
+				$sql="SELECT * FROM $table WHERE $where $order LIMIT $start, $finish";
 				$stmt=$this->link->prepare($sql);
 				return $stmt;
 			else:
-				$sql="SELECT * FROM $table LIMIT $start, $finish";
+				$sql="SELECT * FROM $table $order LIMIT $start, $finish";
 				$stmt=$this->link->prepare($sql);
 				return $stmt;
 			endif;
@@ -233,16 +263,39 @@ class Apps extends Database
 		}
 
 	}
-	public function pagination($name, $table, $number, $where=null)
+	public function selectTable_limit2($table, $start, $finish, $where=null, $getData=NULL, $order=null)
+	{
+		try
+		{
+			if(!empty($where)):
+				$sql="SELECT * FROM $table WHERE $where=:$where $order LIMIT $start, $finish";
+				$stmt=$this->link->prepare($sql);
+				$stmt->bindParam(":$where",$getData);
+				
+				return $stmt;
+			else:
+				$sql="SELECT * FROM $table $order LIMIT $start, $finish";
+				$stmt=$this->link->prepare($sql);
+				return $stmt;
+			endif;
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+
+	}
+	public function pagination($name, $table, $number, $where=null, $order=null)
 	{
 		
 		$finish=$number;
 		$page=isset($_GET[$name])? (int)$_GET[$name]:1;
 		$start=($page>1) ? ($page * $finish) - $finish:0;
-		$stmt=(!empty($where))?$this->selectTable_limit($table,$start, $finish, $where):$this->selectTable_limit($table,$start,$finish);		
+		$stmt=(!empty($where))?$this->selectTable_limit($table,$start, $finish, $where, $order):$this->selectTable_limit($table,$start,$finish,NULL,$order);		
 		$stmt->execute();
 		return $stmt;
 	}
+	
 	public function paginationNumber($table, $number, $url=null, $name=null, $where=null)
 	{
 		$nopage = isset($_GET[$name])? (int)$_GET[$name]:1;	
@@ -281,26 +334,126 @@ class Apps extends Database
 			}
 		}
 
-		echo($nopage==$jumpage)?NULL:'<li><a href="?'.$url.'&halaman='.($nopage+1).'" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		echo($nopage>=$jumpage)?NULL:'<li><a href="?'.$url.'&halaman='.($nopage+1).'" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
 		echo '</ul></nav>';
 
 	
 	}
-	public function searchData($tabel, $kolom1, $data, $kolom2=null)
+	public function pagination2($name, $table, $number, $where=NULL, $getData=NULL, $order=null)
+	{
+		
+		$finish=$number;
+		$page=isset($_GET[$name])? (int)$_GET[$name]:1;
+		$start=($page>1) ? ($page * $finish) - $finish:0;
+		$stmt=(!empty($where))?$this->selectTable_limit2($table,$start, $finish, $where, $getData, $order):$this->selectTable_limit2($table, $start,$finish, NULL, $getData, $order);		
+		$stmt->execute();
+		return $stmt;
+	}
+	public function paginationNumber2($table, $number, $url=null, $name=null, $where=null, $getData=NULL, $order=null)
+	{
+		$nopage = isset($_GET[$name])? (int)$_GET[$name]:1;	
+		$stmt=$this->selectTable2($table, $where, $getData);
+		(!empty($where))?$stmt->execute():'';
+		$name_order=(!empty($_GET['name']))?'&name='.$_GET['name'].'':NULL;
+		$order=(!empty($_GET['order']))?'&order='.$_GET['order'].'':NULL;
+		$total=$stmt->rowCount();
+		$jumpage=ceil($total/$number);
+		
+		echo'<nav aria-label="Page navigation"><ul class="pagination">';
+		echo ($nopage>1)?'<li><a href="?'.$url.'&halaman='.($nopage-1).'" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>':NULL;
+			
+		
+		for($page=1; $page<=$jumpage; $page++)
+		{
+			
+			
+
+			if(($page>=$nopage-2)&&($page<=$nopage+2)||($page==1)||($page==$jumpage))
+			{
+				//$tampilpage = $page;
+				//$r=($tampilpage==1) && ($tampilpage!=2)? '..':NULL;
+				//$x=($nopage==($jumpage-1)) || ($nopage==$jumpage)?'.':NULL;
+				/**
+				if(($nopage==($jumpage-1)) || ($nopage==$jumpage)){
+					echo '.';
+				}else{
+					NULL;
+				}	
+
+				**/		
+				
+				$class=($page==$nopage)?'active':NULL;
+				echo '<li class="'.$class.'"><a href="?'.$url.$name_order.'&halaman='.$page.'">'.$page.'</a></li>';
+
+				
+			}
+		}
+
+		echo($nopage>=$jumpage)?NULL:'<li><a href="?'.$url.$name_order.$order.'&halaman='.($nopage+1).'" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		echo '</ul></nav>';
+
+	
+	}
+	public function paginationNumberBootstrap($table, $number, $url=null, $name=null, $where=null)
+	{
+		$nopage = isset($_GET[$name])? (int)$_GET[$name]:1;	
+		$stmt=$this->selectTable($table, $where);
+		(!empty($where))?$stmt->execute():'';
+		$total=$stmt->rowCount();
+		$jumpage=ceil($total/$number);
+		
+		echo'<nav aria-label="Page navigation example"><ul class="pagination justify-content-center">';
+		echo ($nopage>1)?'<li class="page-item"><a class="page-link" href="?'.$url.'&halaman='.($nopage-1).'" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>':NULL;
+			
+		
+		for($page=1; $page<=$jumpage; $page++)
+		{
+			
+			
+
+			if(($page>=$nopage-2)&&($page<=$nopage+2)||($page==1)||($page==$jumpage))
+			{
+				$tampilpage = $page;
+				$r=($tampilpage==1) && ($tampilpage!=2)? '..':NULL;
+				$x=($nopage==($jumpage-1)) || ($nopage==$jumpage)?'.':NULL;
+				/**
+				if(($nopage==($jumpage-1)) || ($nopage==$jumpage)){
+					echo '.';
+				}else{
+					NULL;
+				}	
+
+				**/		
+				
+				$class=($page==$nopage)?'active':'page-item';
+				echo '<li class="'.$class.'"><a class="page-link" href="?'.$url.'&halaman='.$page.'">'.$page.'</a></li>';
+
+			
+			}
+		}
+
+		echo($nopage>=$jumpage)?NULL:'<li class="page-item"><a class="page-link" href="?'.$url.'&halaman='.($nopage+1).'" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		echo '</ul></nav>';
+
+	
+	}
+	
+	public function searchData($tabel, $kolom1, $data, $kolom2=null, $limit=null)
 	{
 		try
 		{
 			
 
-		
+			$keyword="$data%";
 			if(!empty($kolom2)):
-				$sql="SELECT * FROM $tabel WHERE $kolom1 LIKE '$data%' OR $kolom2 LIKE '$data%'";
-				$stmt=$this->link->prepare($sql);
+				$sql="SELECT * FROM $tabel WHERE $kolom1 LIKE '$data%' OR $kolom2 LIKE '$data%' $limit";
+				$stmt=$this->link->prepare($sql);				
+				//$stmt->bindParam(":$data",$keyword, PDO::PARAM_STR);
+				$stmt->execute();				
 				return $stmt;
 			else:
-				$sql="SELECT * FROM $tabel WHERE $kolom1 LIKE '$data%'";
+				$sql="SELECT * FROM $tabel WHERE $kolom1 LIKE '$data%' $limit";
 				$stmt=$this->link->prepare($sql);
-				$stmt->execute();
 				return $stmt;
 			endif;
 			
@@ -311,6 +464,7 @@ class Apps extends Database
 		}
 
 	}
+
 	
 	public function __destruct()
 	{
