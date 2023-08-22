fbe24dc547fc39a859ada7e038cae324b7267030
Ahmad Zaelani <myroot593@gmail.com>
2023-08-22 11:54:21 +0700

    Update Splautoload.php

--- app/core/Splautoload.php
+++ app/core/Splautoload.php
@@ -16,24 +16,27 @@
 	* Anda hanya perlu menuliskannya seperti berikut 
 	* spl_autoload_register(array('namaclass','nama_method'))
 	* atau Anda membuat daftar path module didalam array dengan looping.
-	* Contoh :
 	
-		spl_autoload_register(function($className)
+
+
+**/
+
+
+spl_autoload_register(function($className)
 		{
 
-			$list_direkotori = array
+			$list_direktori = array
 			(
-				'proja/',
-				'projb/',
-				'projc/',
+				'/',
+				'/../../database/',			
+				'/../load/',
 
 			);
 			foreach($list_direktori as $list)
 			{
-				if(file_exists($list.'file_class.'.strtolower($className).'.php'))
-				{
+				if(is_readable(__DIR__.$list.$className.'.php'))			{
 		
-					require_once($list.'file_class.'.strtolower($className).'.php');
+					require_once(__DIR__.$list.$className.'.php');
 				}
 				
 			}
@@ -41,123 +44,22 @@
 		});
 
 
-**/
 
 
-function autoloadDatabase($className)
-{
-	
-	$filename =__DIR__.'/../../database/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-
-}
-function autoloadCore($className)
-{
-	
-	$filename =__DIR__.'/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-
-}
-function autoloadView($className)
-{
-	$filename =__DIR__.'/../load/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-}
-function autoloadAjax($className)
-{
-	$filename =__DIR__.'/../load/Ajax/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-}
-function handler($className)
-{
-	$filename =__DIR__.'/../auth/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-}
-function autoloadPublic($className)
-{
-	$filename =__DIR__.'/../public/'.$className.'.php';
-	if(is_readable($filename))
-	{
-		require_once $filename;
-	}
-}
-
-spl_autoload_register("autoloadDatabase");
-spl_autoload_register("autoloadCore");
-spl_autoload_register("autoloadView");
-spl_autoload_register("autoloadAjax");
-spl_autoload_register("handler");
-spl_autoload_register("autoloadPublic");
-
-
-$handler = new Auth($databases);
 $db = new Database($databases);
 $obj = new Apps($databases);
 $obj2 = new Appscostum($databases);
 $app = new Property;
-$theme = new Template($obj);
-$upload = new Uploadclass;
-/**
- * Contoh - contoh yang pernah di implenebtasikan dalam aplikasi ezakat
- * 
-	$dashboard = new Dashboard($obj2, $theme, $app);
-	$pengguna = new Useradmin($obj, $app, $salt, $upload);
-	$profile = new Userprofile($obj, $app, $salt, $upload);
-
-
-	$pengelola = new Pengelola($obj, $app, $obj2);
-	$zakat = new Zakat($obj, $app, $obj2);
-	$penerima = new Penerima($obj, $app, $obj2);
-	$penyaluran = new Penyaluran($obj, $obj2, $app);
-	$jeniszakat = new Jeniszakat($obj, $app, $obj2);
-	$jenisbayar = new Jenispembayaran($obj, $app, $obj2);
-
-
-	$kec = new Alkec($obj, $app, $obj2);
-
-	$setting = new Settingweb($obj, $app, $upload);
-
-
-	$modal = new Modal;
-	$modaljs = new Modaljs;
-	$optionjs = new Ajaxoptionjs;
-
-	
-	$pbctheme = new Publictemplate($obj);
-	$pbcpengelola = new Publicpengelola($obj, $obj2,  $app, $pbctheme);
-	$pbczakat = new Publiczakat($obj, $obj2, $app, $pbctheme);
-	$pbcpenyaluran = new Publicpenyaluran($obj, $obj2, $app, $pbctheme);
-	$pbcpenerima = new Publicpenerima($obj, $obj2, $app, $pbctheme);
-	$pbcdashboard = new Publicdashboard($obj2, $pbctheme, $app);
-	$verify = new Verifyuser($obj, $obj2,  $app, $pbctheme);
-
-	$login = new Loginuser($obj, $obj2, $app, $salt, $pbctheme);
-**/
-
+$theme = new Template();
 
 $modul = new Moduleload
 (
 	
 	$obj, 
 	$obj2,
-	$login,
 	$app, 
-	$theme
+	$theme,
+
 	
 );
 
