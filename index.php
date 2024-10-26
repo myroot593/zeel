bee26ab3bcae95258d0a4f147228fec32e94622b
Ahmad Zaelani <myroot593@gmail.com>
2024-10-26 10:08:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-require "database/Settings.php";
-require "app/core/Splautoload.php";
-
-$modul->test();
-
-?>
-
+session_start();
+require_once __DIR__ .'/app/Autoloader/Psr4Autoloader.php';
+//set autoloder from composer if u want
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/config/routes.php';
